ef4eb7caf36dcc7ebc67e6267a29939c3a281540
Yanagisawa_Kenta <edf0482@gmail.com>
2022-11-19 16:57:16 +0900

    I fixed AOJ2's programs

--- AOJ/02/02_C.py
+++ AOJ/02/02_C.py
@@ -1,14 +1,3 @@
-a, b, c = map(int, input().split())
-if a > b:
-    s = a
-    a = b
-    b = s
-if b > c:
-    s = b
-    b = c
-    c = s
-if a > b:
-    s = a
-    a = b
-    b = s
-print(a, b, c)
+n = list(map(int, input().split()))
+n.sort()
+print(*n)
--- AOJ/02/02_D.py
+++ AOJ/02/02_D.py
@@ -1,15 +1,5 @@
 W, H, x, y, r = map(int, input().split())
-flag_x = False
-flag_y = False
-if (0 <= x-r and x+r <= W):
-    flag_x = True
-else:
-    flag_x = False
-if (0 <= y-r and y+r <= H):
-    flag_y = True
-else:
-    flag_y = False
-if (flag_x and flag_y):
+if (0 <= x-r and x+r <= W and 0 <= y-r and y+r <= H):
     print("Yes")
 else:
     print("No")
